1bb95c149d1dc4e1667c76c68f97326646658efb
Matheus <xturax@gmail.com>
2019-06-26 05:11:58 -0300

    fix: bits and pins order
    - fix: bits order
    - IC signal order
    - added nanoseconds time delay

--- src/decoder.c
+++ src/decoder.c
@@ -1,7 +1,7 @@
 /*
  * ============================================================================
  *
- *       Filename:  main.c
+ *       Filename:  decoder.c
  *
  *    Description:  Decoder file of the project
  *
@@ -20,6 +20,7 @@
 #include <stdio.h>
 #include "gpio_table.h"
 #include "gpio.h"
+#include <time.h>
 //#include <galileo2io.h>
 
 #include "decoder.h"
@@ -28,43 +29,53 @@
 int read_decoder() {
   char data[2];
   int decode_value = 0;
-
-  read_pin(D0_PIN, data);
-  decode_value += atoi(data);
-  read_pin(D1_PIN, data);
-  decode_value += atoi(data)*2;
-  read_pin(D2_PIN, data);
-  decode_value += atoi(data)*4;
-  read_pin(D3_PIN, data);
-  decode_value += atoi(data)*8;
-  read_pin(D4_PIN, data);
-  decode_value += atoi(data)*16;
-  read_pin(D5_PIN, data);
-  decode_value += atoi(data)*32;
-  read_pin(D6_PIN, data);
-  decode_value += atoi(data)*64;
-  read_pin(D7_PIN, data);
-  decode_value += atoi(data)*128;
+  struct timespec = sleep_time;
 	
-  // Set OE and SEL pin
-  set_pin(OE_PIN, GPIO_LOW, GPIO_IN);	
-  set_pin(SEL_PIN, GPIO_HIGH, GPIO_IN);
-  set_pin(OE_PIN, GPIO_HIGH, GPIO_IN);
+  sleep_time.tv_sec = 0;
+  sleep_time.tv_nsec = 90; //1000 ns = 1 us
 
-  read_pin(D0_PIN, data);
-  decode_value += atoi(data);
-  read_pin(D1_PIN, data);
+  // Set OE and SEL pin into HIGH BYTE read state
+  set_pin(OE_PIN, GPIO_LOW, GPIO_IN);	
+  set_pin(SEL_PIN, GPIO_LOW, GPIO_IN);	
+  nanosleep(&slptm,NULL);
+	
+  read_pin(D7_PIN, data);
   decode_value += atoi(data)*256;
-  read_pin(D2_PIN, data);
+  read_pin(D6_PIN, data);
   decode_value += atoi(data)*512;
-  read_pin(D3_PIN, data);
+  read_pin(D5_PIN, data);
   decode_value += atoi(data)*1024;
   read_pin(D4_PIN, data);
   decode_value += atoi(data)*2048;
-  read_pin(D5_PIN, data);
+  read_pin(D3_PIN, data);
   decode_value += atoi(data)*4096;
-  read_pin(D6_PIN, data);
+  read_pin(D2_PIN, data);
   decode_value += atoi(data)*8192;
-  read_pin(D7_PIN, data);
+  read_pin(D1_PIN, data);
   decode_value += atoi(data)*16384;
+  read_pin(D0_PIN, data);
+  decode_value += atoi(data)*32768;	
+
+  // Set OE and SEL pin into LOW BYTE read state	
+  set_pin(SEL_PIN, GPIO_HIGH, GPIO_IN);	
+  nanosleep(&slptm,NULL);
+
+  read_pin(D7_PIN, data);
+  decode_value += atoi(data);
+  read_pin(D6_PIN, data);
+  decode_value += atoi(data)*2;
+  read_pin(D5_PIN, data);
+  decode_value += atoi(data)*4;
+  read_pin(D4_PIN, data);
+  decode_value += atoi(data)*8;
+  read_pin(D3_PIN, data);
+  decode_value += atoi(data)*16;
+  read_pin(D2_PIN, data);
+  decode_value += atoi(data)*32;
+  read_pin(D1_PIN, data);
+  decode_value += atoi(data)*64;
+  read_pin(D0_PIN, data);
+  decode_value += atoi(data)*128;
+	
+  set_pin(OE_PIN, GPIO_HIGH, GPIO_IN);
 }

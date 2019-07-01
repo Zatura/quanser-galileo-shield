210b4e50303abbb859ef6d9d26bc9c307512c34f
Zatura <xturax@gmail.com>
2019-06-30 23:16:36 -0300

    move_to_angle function updated

--- src/motion.c
+++ src/motion.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "decoder.h"
+#include "control.h"
+#include "gpio_table.h"
 
 int move(float angle, int time){
   printf("angle: %f\n", angle);
@@ -10,9 +15,22 @@ int move(float angle, int time){
 }
 
 int move_to_angle(float angle){
+  pid_control *pid = (pid_control*)malloc(sizeof(pid_control));
+  clock_t last, now;
+  float current_position = 0;
+  float error = 0;
+
+  set_gains(pid, 1, 0, 0);
+
+  last = clock();
   printf("angle: %f\n", angle);
+  printf("error: %f\n", error);
   while(1){
-    //TODO
+    current_position = 360*read_decoder()/QNSR_RESOLUTION;
+    error = angle - current_position;
+    now = clock();
+    update_voltage(pid, error, (now-last)/CLOCKS_PER_SEC);
+    last = now;
   }
   return 0;
 }

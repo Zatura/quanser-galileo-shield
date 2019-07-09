523a824da637d8cb8933bc1a12cc551f1f57613f
Arthur Ribeiro <arthur_ribeiro04@hotmail.com>
2019-07-09 19:38:29 -0300

    move to target ready to use

--- src/motion.c
+++ src/motion.c
@@ -14,10 +14,14 @@ int move(float voltage){
   voltage = fminf(27, voltage);
   voltage = fmaxf(-27, voltage);
   float duty_cycle = 50 + 50*(voltage)/27;
+
+  duty_cycle = fminf(97, duty_cycle);
+  duty_cycle = fmaxf(2, duty_cycle);
   set_duty_cycle(duty_cycle);
 
   // shutdown-pin low: Turn on the bridge drivers
   write_pin(SD_PIN, GPIO_LOW);
+  enable_pwm();
   return 0;
 }
 
@@ -37,7 +41,7 @@ int move_to_angle(float angle){
     while(1){
       current_position = 360*read_decoder()/QNSR_RESOLUTION;
       error = angle - current_position;
-      if (print_count % 100 == 0)
+      if (print_count % 10 == 0)
        printf("%d: ERR %f | POS %f | VOL %f\n",print_count, error, current_position, pid->voltage);
       now = clock();
       time_elapsed = now*2.8/CLOCKS_PER_SEC;
@@ -46,7 +50,6 @@ int move_to_angle(float angle){
       last = now;
       print_count++;
     }
-    return 0;
   };
   return 1;
 }
@@ -55,4 +58,5 @@ void stop(){
   set_duty_cycle(50);
   usleep(100);
   write_pin(SD_PIN, GPIO_HIGH);
+  disable_pwm();
 }
